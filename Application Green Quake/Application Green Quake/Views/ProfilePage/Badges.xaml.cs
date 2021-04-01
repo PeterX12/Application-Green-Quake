25f795a2dd19de25b81c01017af1690c175fd565
Peter Lucan <peteroluc@gmail.com>
2021-04-01 16:57:26 +0100

    Badges First Update

--- Application Green Quake/Application Green Quake/Views/ProfilePage/Badges.xaml.cs
+++ Application Green Quake/Application Green Quake/Views/ProfilePage/Badges.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Application_Green_Quake.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Firebase.Storage;
+using Rg.Plugins.Popup.Services;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,38 @@ namespace Application_Green_Quake.Views.ProfilePage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Badges : ContentPage
     {
+        IAuth auth;
+        int numOfLogs = 0;
         public Badges()
         {
             InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+            OnAppearing();
         }
+
+        protected async override void OnAppearing()
+        {
+            FirebaseClient firebaseClient = new FirebaseClient("https://application-green-quake-default-rtdb.firebaseio.com/");
+            auth = DependencyService.Get<IAuth>();
+
+            numOfLogs = (await firebaseClient
+            .Child("EnergyPoints")
+            .Child(auth.GetUid())
+            .OnceSingleAsync<EnergyPoints>()).numberOfLogs;
+
+            if (numOfLogs == 4)
+            {
+                a1.Source = await new FirebaseStorage("application-green-quake.appspot.com")
+                .Child("Badges")
+                .Child("testBadge.png")
+                .GetDownloadUrlAsync();
+            }
+        }
+
+        private async void NavigateToBadgePopUp(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new BadgePopUp());
+        }
+
     }
 }

0bae143ae4876caf5c587110a93c4a850e5f3f76
Peter Lucan <peteroluc@gmail.com>
2021-04-26 03:16:56 +0100

    Git Trophies Added. Project Complete

--- Application Green Quake/Application Green Quake/Views/ProfilePage/TrophyCase.xaml.cs
+++ Application Green Quake/Application Green Quake/Views/ProfilePage/TrophyCase.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Application_Green_Quake.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +16,31 @@ namespace Application_Green_Quake.Views.ProfilePage
         {
             InitializeComponent();
             CurrentPage = Children[tab];
+            OnAppearing();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (GetData.points >= 1000)
+            {
+                t1.Source = ImageSource.FromResource("Application_Green_Quake.Images.Trophies.diamond.png");
+                t1Txt.Text = "Diamond Trophy";
+            }
+            if (GetData.points >= 500)
+            {
+                t2.Source = ImageSource.FromResource("Application_Green_Quake.Images.Trophies.gold.png");
+                t2Txt.Text = "Gold Trophy";
+            }
+            if (GetData.points >= 250)
+            {
+                t3.Source = ImageSource.FromResource("Application_Green_Quake.Images.Trophies.silver.png");
+                t3Txt.Text = "Silver Trophy";
+            }
+            if (GetData.points >= 100)
+            {
+                t4.Source = ImageSource.FromResource("Application_Green_Quake.Images.Trophies.bronze.png");
+                t4Txt.Text = "Bronze Trophy";
+            }
         }
     }
 }
